1b7212ba067ee7765b9bf095261284f9a7622b31
nilushim <nilufarsaidova281@gmail.com>
2025-04-24 15:17:56 +0500

    first commit

---
+++ quiz/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Language, Question, Answer, UserResult
+
+admin.site.register(Language)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(UserResult)
+
+
+
+# Register your models here.
